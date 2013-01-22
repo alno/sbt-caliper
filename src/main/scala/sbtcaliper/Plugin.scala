c4ab003f6bb78bce0706cb9893e7b1da05e879cf
Alexey Noskov <alexey.noskov@gmail.com>
2013-01-22 22:33:29 +0400

    Benchmark-only task

--- src/main/scala/sbtcaliper/Plugin.scala
+++ src/main/scala/sbtcaliper/Plugin.scala
@@ -1,19 +1,39 @@
 package sbtcaliper
 
 import sbt._
-import sbt.Defaults._
 import sbt.Keys._
+import sbt.Defaults._
 
 object Plugin extends sbt.Plugin {
 
   val benchmark = TaskKey[Unit]("benchmark", "Executes all benchmarks.")
+  val benchmarkOnly = InputKey[Unit]("benchmark-only", "Executes specified benchmarks.")
 
   val benchmarkTasks = Seq(
-    benchmark <<= (fullClasspath in Test, sources in Test, scalaInstance, javaHome, javaOptions, baseDirectory, outputStrategy, streams) map {
-      (cpa, srcs, si, jhome, jopts, dir, strategy, s) =>
-        val cp = "-classpath" :: Path.makeString(cpa.files) :: Nil
-        val args = srcs map { _.base } filter { _.endsWith("Benchmark") } toArray
+    benchmark <<= benchmarkTaskInit.zip(sources in Test) {
+      case (runTask, srcsTask) =>
+        (runTask :^: srcsTask :^: KNil) map {
+          case run :+: srcs :+: HNil =>
+            run { srcs map { _.base } filter { _.endsWith("Benchmark") } }
+        }
+    },
 
+    benchmarkOnly <<= sbt.inputTask { (argTask: TaskKey[Seq[String]]) =>
+      benchmarkTaskInit.zip(argTask) {
+        case (runTask, argTask) =>
+          (runTask :^: argTask :^: KNil) map {
+            case run :+: args :+: HNil =>
+              run { args }
+          }
+      }
+    })
+
+  override val settings = benchmarkTasks
+
+  private def benchmarkTaskInit: Project.Initialize[Task[Seq[String] => Unit]] =
+    (fullClasspath in Test, scalaInstance, javaHome, javaOptions, baseDirectory, outputStrategy, streams) map {
+      (cpa, si, jhome, jopts, dir, strategy, s) =>
+        val cp = "-classpath" :: Path.makeString(cpa.files) :: Nil
         val fr = new ForkRun(
           ForkOptions(scalaJars = si.jars,
             javaHome = jhome,
@@ -21,9 +41,12 @@ object Plugin extends sbt.Plugin {
             outputStrategy = strategy,
             workingDirectory = Some(dir)))
 
-        sbt.toError(fr.run("com.google.caliper.Runner", Build.data(cpa), args, s.log))
-    })
-
-  override val settings = benchmarkTasks
+        { args: Seq[String] =>
+          if (args.isEmpty)
+            println("No benchmarks specified - nothing to run")
+          else
+            sbt.toError(fr.run("com.google.caliper.Runner", Build.data(cpa), args, s.log))
+        }
+    }
 
 }
